4e03b2d769d1ba64e4ecfb1c77036cb9561c8c26
Martin H. J. Bramwell <mhb.warehouseman@gmail.com>
2015-03-29 17:01:11 -0400

    enable configuring launch and execution time settings all from the same file

--- qikRun.sh
+++ qikRun.sh
@@ -1,36 +1,80 @@
 #!/bin/bash
 #
-if [ ! -f "config.sh" ]; then
-  echo "Please copy 'example.config.sh' to 'config.sh' and make any necessary corrections. "
-  echo "cp example.config.sh config.sh "
-  exit
-fi
-source config.sh
-echo "### Configuration is :"
-echo "   ~  Connect to  Mongo server : " $MONGO_URL
-echo "   ~  Running in demo mode : " $DEMO_MODE
-echo "### ~   ~   ~    "
+###################   Functions
+function weCanParseJSON { 
+  jq --version >/dev/null 2>&1
+  if [[ 0 -ne $? ]]; then
+    echo ""
+    echo "You need the JSON parser 'jq' if you want to be able to set meteor command line parameters from your settings.json file."
+    echo "Please execute the following command : "
+    echo "sudo apt-get -y install jq"
+    exit
+  fi
+  if [ ! -f "settings.json" ]; then
+    echo "Please copy 'example.settings.json' to 'settings.json' and make any necessary corrections. "
+    echo "cp example.settings.json settings.json "
+    exit
+  fi
+}
 
+function parseJSON {
+  local __PARM=$1
+  local TMP=$(cat settings.json | jq .$1 | sed "s/\"//g")
+  eval $__PARM="'${TMP}'"
+}
 
-if [ ${MOCHA} ]; then
-  meteor add mike:mocha
-  echo "mocha"
-else
-  meteor remove mike:mocha
-  echo "NO mocha"
-fi
+function parseJSON_public {
+  local __PARM=$1
+  local TMP=$(cat settings.json | jq .public.$1 | sed "s/\"//g")
+  eval $__PARM="'${TMP}'"
+}
 
-if [ ${CUCUMBER} ]; then
-  meteor add xolvio:webdriver
-  meteor add xolvio:cucumber
-  echo "cucumber"
-else
-  meteor remove xolvio:webdriver
-  meteor remove xolvio:cucumber
-  echo "NO cucumber"
-fi
+function addOrRemoveMocha {
+  parseJSON MOCHA
+  if [ ${MOCHA} == "yes" ]; then
+    meteor add mike:mocha
+    echo "Mocha added"
+  else
+    meteor remove mike:mocha
+    echo "Mocha removed"
+  fi
 
+}
+
+function addOrRemoveCucumber {
+  parseJSON CUCUMBER
+  if [ ${CUCUMBER} == "yes" ]; then
+    meteor add xolvio:webdriver
+    meteor add xolvio:cucumber
+    echo "Cucumber added"
+  else
+    meteor remove xolvio:webdriver
+    meteor remove xolvio:cucumber
+    echo "Cucumber removed"
+  fi
+}
+
+###################   Main Program
+#
+weCanParseJSON
+parseJSON MONGO_PORT
+parseJSON MONGO_SERVER
+parseJSON MONGO_DB
+export MONGO_URL=mongodb://${MONGO_SERVER}:${MONGO_PORT}/${MONGO_DB}
+#
+addOrRemoveMocha
+addOrRemoveCucumber
+#
+parseJSON demo_mode
+#
+echo "### Configuration is . . . "
+echo "   ~  Connect to  Mongo server : " ${MONGO_URL}
+echo "   ~      Running in demo mode : " ${demo_mode}
+echo "   ~  Velocity "
+echo "   ~                     Mocha : " ${MOCHA}
+echo "   ~                  Cucumber : " ${CUCUMBER}
+echo "### ~   ~   ~   ~   ~   ~   ~   ~   ~   ~    "
+#
 meteor
-
 
 
